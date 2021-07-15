af6968a55aa8414946bd5a779a479d5c5d863378
Kyriet <kyriet.sebastian@gmail.com>
2021-07-16 00:15:36 +0200

    Add loading index file

--- src/snapshot/index.rs
+++ src/snapshot/index.rs
@@ -1,6 +1,7 @@
+use std::borrow::Borrow;
 use std::fs::File;
-use std::io::{self, BufWriter, Write};
-use std::path::PathBuf;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::path::{Path, PathBuf};
 
 use super::timestamp::Timestamp;
 
@@ -19,6 +20,25 @@ impl Index {
             entries: vec![],
         }
     }
+
+    #[allow(dead_code)]
+    pub fn open(path: &Path, timestamp: &Timestamp) -> Result<Self, String> {
+        let file = File::open(path).or(Err("Cannot open index.txt"))?;
+        let file = BufReader::new(&file);
+        let mut entries = Vec::new();
+        for line in file.lines() {
+            let line = line.or(Err("Error while reading index.txt"))?;
+            let index_entry = IndexEntry::from_line(line.borrow()).ok_or("index.txt is broken")?;
+            entries.push(index_entry);
+        }
+        let index = Index {
+            timestamp: timestamp.clone(),
+            location: path.to_owned(),
+            entries,
+        };
+        Ok(index)
+    }
+
     pub fn push(&mut self, timestamp: &Timestamp, path: PathBuf) {
         self.entries.push(IndexEntry {
             timestamp: timestamp.to_owned(),
@@ -43,8 +63,76 @@ struct IndexEntry {
     path: PathBuf,
 }
 
+impl IndexEntry {
+    fn from_line(line: &str) -> Option<Self> {
+        let (timestamp_slice, path_slice) = line.split_once(' ')?;
+        let entry = Self {
+            timestamp: Timestamp::from(timestamp_slice)?,
+            path: PathBuf::from(path_slice.trim()),
+        };
+        Some(entry)
+    }
+}
+
 impl ToString for IndexEntry {
     fn to_string(&self) -> String {
         format!("{} {}", self.timestamp, self.path.display())
+    }
+}
+
+#[cfg(test)]
+mod index_tests {
+    use super::*;
+
+    #[test]
+    fn open_index_file() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let file_path = tempdir.path().join("index.txt");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "2021-07-16_18.34 /some path/with spaces").unwrap();
+        writeln!(file, "2021-07-17_18.34 /another path").unwrap();
+
+        let timestamp = Timestamp::from("2021-07-15_18.34").unwrap();
+        let index = Index::open(&file_path, &timestamp).unwrap();
+
+        assert_eq!(timestamp.to_string(), index.timestamp.to_string());
+        assert_eq!(file_path, index.location);
+        assert_eq!(index.entries[0].path, Path::new("/some path/with spaces"));
+        assert_eq!(index.entries[1].path, Path::new("/another path"));
+        assert_eq!(index.entries[0].timestamp.to_string(), "2021-07-16_18.34");
+        assert_eq!(index.entries[1].timestamp.to_string(), "2021-07-17_18.34");
+    }
+
+    #[test]
+    fn open_invalid_file_returns_none() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let file_path = tempdir.path().join("index.txt");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "foo").unwrap();
+
+        let timestamp = Timestamp::from("2021-07-15_18.34").unwrap();
+        let result = Index::open(&file_path, &timestamp);
+        assert!(result.is_err());
+    }
+}
+
+#[cfg(test)]
+mod index_entry_tests {
+    use super::*;
+
+    #[test]
+    fn index_entry_from_valid_line() {
+        let valid_line = "2021-07-15_18.34 /dir 1/file.txt\n";
+
+        let index_entry = IndexEntry::from_line(valid_line).unwrap();
+        assert_eq!("2021-07-15_18.34", format!("{}", index_entry.timestamp));
+        assert_eq!("/dir 1/file.txt", format!("{}", index_entry.path.display()));
+    }
+
+    #[test]
+    fn index_entry_from_invalid_line() {
+        assert!(IndexEntry::from_line("foo").is_none());
+        // invalid timestamp
+        assert!(IndexEntry::from_line("2021-07-15_18:34  /dir_1/file.txt\n").is_none());
     }
 }
--- src/snapshot/timestamp.rs
+++ src/snapshot/timestamp.rs
@@ -2,15 +2,21 @@ use std::fmt::Display;
 
 #[derive(PartialEq, PartialOrd, Clone)]
 pub struct Timestamp {
-    inner: chrono::DateTime<chrono::Local>,
+    inner: chrono::NaiveDateTime,
 }
 
 impl Timestamp {
     pub fn now() -> Self {
         Self {
-            inner: chrono::offset::Local::now(),
+            inner: chrono::offset::Local::now().naive_local(),
         }
     }
+
+    pub fn from(str: &str) -> Option<Self> {
+        let inner = chrono::NaiveDateTime::parse_from_str(str, "%Y-%m-%d_%H.%M").ok()?;
+        Some(Self { inner })
+    }
+
     pub fn get_next(&self) -> Self {
         let next_date_time = self.inner + chrono::Duration::minutes(1);
         Self {
@@ -33,5 +39,30 @@ impl Display for Timestamp {
             time.hour(),
             time.minute()
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Datelike, Timelike};
+
+    use super::*;
+
+    #[test]
+    fn get_timestamp_from_string() {
+        let ts = Timestamp::from("2021-07-15_18.34").unwrap();
+
+        assert_eq!(ts.inner.year(), 2021);
+        assert_eq!(ts.inner.month(), 7);
+        assert_eq!(ts.inner.day(), 15);
+        assert_eq!(ts.inner.hour(), 18);
+        assert_eq!(ts.inner.minute(), 34);
+    }
+
+    #[test]
+    fn timestamp_from_invalid_string_returns_none() {
+        assert!(Timestamp::from("boo").is_none());
+        assert!(Timestamp::from(" \t  2021-07-15_18.34  \t\n").is_none());
+        assert!(Timestamp::from("2021-07-15 18:34").is_none());
     }
 }
